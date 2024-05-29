9f3df296359128112f84b2c041a3394a64ff9e6c
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-05-28 20:04:02 -0400

    Update script.js, Add script.test.js, Updated form.html, Update index.html
    - Update script.js with smooth scrolling, navbar collapse toggle, form submission handling, and other enhancements
    
    - Add unit tests for script.test.js
    
    - Updated form.html with improved styling and added form validation.
    
    - Update index.html with new content and styling

--- script.js
+++ script.js
@@ -1,183 +1,229 @@
 
 
-    // Smooth scrolling for navigation links
-    $('.navbar-nav a').on('click', function(event) {
-        if (this.hash !== '') {
-            event.preventDefault();
-            const hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-                window.location.hash = hash;
-            });
+/**
+ * Smooth scrolling for navigation links
+ * @param {Event} event - The click event
+ */
+$('.navbar-nav a').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+    }
+});
+
+/**
+ * Navbar collapse toggle effect
+ * @param {Event} event - The click event
+ */
+$('.navbar-toggler').on('click', function () {
+    $('.navbar-collapse').toggleClass('show');
+});
+
+/**
+ * Close navbar when a link is clicked
+ * @param {Event} event - The click event
+ */
+$('.navbar-nav a').on('click', function () {
+    $('.navbar-collapse').removeClass('show');
+});
+
+/**
+ * Scale effect on hover
+ * @param {Event} event - The hover event
+ */
+$(".quarter").hover(function () {
+    $(this).css("transform", "scale(1.2)");
+}, function () {
+    $(this).css("transform", "scale(1)");
+});
+
+/**
+ * Smooth scroll to booking section
+ * @param {Event} event - The click event
+ */
+$(".smooth-scroll").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#booking").offset().top
+    }, 800);
+});
+
+/**
+ * Set the minimum date to the current date
+ */
+var currentDate = new Date();
+var day = ("0" + currentDate.getDate()).slice(-2);
+var month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
+var today = currentDate.getFullYear() + "-" + (month) + "-" + (day);
+$('#date').attr('min', today);
+
+/**
+ * Form submission handling
+ * @param {Event} event - The form submission event
+ * @returns {boolean} - Returns false to prevent the default form submission
+ */
+$(".booking-form").submit(function (event) {
+    event.preventDefault();
+    console.log('Got Here...');
+    // Get form values
+    var name = $("#name").val().trim();
+    var email = $("#email").val().trim();
+    var eventType = $("#event-type").val().trim();
+    var date = $("#date").val();
+    var message = $("#message").val().trim();
+
+    // Check if all fields are filled
+    if (name === '' || email === '' || date === '' || message === '') {
+        $('#alert').removeClass('success').addClass('error').html('Please fill all the required fields.').fadeIn();
+        setTimeout(function () { $('#alert').fadeOut() }, 4000);
+        return false;
+    }
+
+    // Check if the selected date is in the future
+    var currentDate = new Date();
+    var selectedDate = new Date(date + 'T00:00:00'); // Reset time to 00:00:00 to avoid timezone issues
+    if (selectedDate <= currentDate) {
+        $('#alert').removeClass('success').addClass('error').html('Please select a future date.').fadeIn();
+        setTimeout(function () { $('#alert').fadeOut() }, 4000);
+        return false;
+    }
+
+    // Check if the email address is valid
+    if (!isValidEmail(email)) {
+        $('#alert').removeClass('success').addClass('error').html('Please enter a valid email address.').fadeIn();
+        setTimeout(function () { $('#alert').fadeOut() }, 4000);
+        return false;
+    }
+
+    emailjs.init("Qur9J272yrutlMj77");
+
+    emailjs.send("service_zeodffs", "template_s6rjjmy", {
+        sendername: "Soundwave Symphony",
+        to: this.email.value,
+        subject: "New Booking Request",
+        message: "Thank you for booking a project with us. We appreciate your interest in working with us. \n\nYour details are as follows:\n\n" +
+            "Name: " + name + "\n" +
+            "Email: " + email + "\n" +
+            "Event Type: " + eventType + "\n" +
+            "Date: " + date + "\n" +
+            "Message: " + message + ".\n\n" +
+            "We will get back to you soon."
+    }).then(function (response) {
+        console.log('SUCCESS!', response.status, response.text);
+        $('#alert').removeClass('error').addClass('success').html('Thank you! We will get back to you soon.').fadeIn();
+        $("#name").val('');
+        $("#email").val('');
+        $("#event-type").val('');
+        $("#date").val('');
+        $("#message").val('');
+        setTimeout(function () { $('#alert').fadeOut() }, 4000);
+        // Clear the form
+    }, function (error) {
+        console.log('FAILED...', error);
+        $('#alert').removeClass('success').addClass('error').html('Error: Something went wrong. Please try again.').fadeIn();
+        setTimeout(function () { $('#alert').fadeOut() }, 4000);
+    });
+
+    return false; // Prevent the default form submission
+});
+
+/**
+ * Email validation
+ * @param {string} email - The email address to validate
+ * @returns {boolean} - Returns true if the email is valid, false otherwise
+ */
+function isValidEmail(email) {
+    var emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    return emailRegex.test(email);
+}
+
+/**
+ * Add to cart button click event
+ */
+$(".add-to-cart").click(function () {
+    let itemName = $(this).siblings("h3").text();
+    showCustomAlert(itemName + " has been added to your cart.");
+    // Here you can also add code to actually add the item to a shopping cart
+    // For example, updating a cart count, or sending an AJAX request to a server
+});
+
+/**
+ * Show custom alert message
+ * @param {string} message - The message to display in the alert
+ */
+function showCustomAlert(message) {
+    $(".shopping-custom-alert").text(message).fadeIn();
+    setTimeout(function () {
+        $(".shopping-custom-alert").fadeOut();
+    }, 3000); // The alert will disappear after 3 seconds
+}
+
+/**
+ * Initialize Materialize components on DOMContentLoaded event
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    $(".sidenav").sidenav({ edge: "right" });
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+    $("select").formSelect();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
         }
     });
 
-    // Navbar collapse toggle effect
-    $('.navbar-toggler').on('click', function() {
-        $('.navbar-collapse').toggleClass('show');
-    });
+    validateMaterializeSelect();
 
-    // Close navbar when a link is clicked
-    $('.navbar-nav a').on('click', function() {
-        $('.navbar-collapse').removeClass('show');
-    });
-
-      $(".quarter").hover(function(){
-        $(this).css("transform", "scale(1.2)");
-    }, function(){
-        $(this).css("transform", "scale(1)");
-    });
-
-            // Smooth scroll to booking section
-       $(".smooth-scroll").click(function() {
-            $('html, body').animate({
-                scrollTop: $("#booking").offset().top
-            }, 800);
-        });
-
-                // Set the minimum date to the current date
-                var currentDate = new Date();
-                var day = ("0" + currentDate.getDate()).slice(-2);
-                var month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
-                var today = currentDate.getFullYear()+"-"+(month)+"-"+(day);
-                $('#date').attr('min', today);
-
-        // Form submission handling
-        $(".booking-form").submit(function(event) {
-            event.preventDefault();
-            console.log('Got Here...');
-                        // Get form values
-                        var name = $("#name").val().trim();
-                        var email = $("#email").val().trim();
-                        var eventType = $("#event-type").val().trim();
-                        var date = $("#date").val();
-                        var message = $("#message").val().trim();
-            
-                        // Check if all fields are filled
-                        if (name === '' || email === '' || date === '' || message === '') {
-                            $('#alert').removeClass('success').addClass('error').html('Please fill all the required fields.').fadeIn();
-                            setTimeout(function(){ $('#alert').fadeOut() }, 4000);
-                            return false;
-                        }
-            
-             // Check if the selected date is in the future
-             var currentDate = new Date();
-             var selectedDate = new Date(date + 'T00:00:00'); // Reset time to 00:00:00 to avoid timezone issues
-             if (selectedDate <= currentDate) {
-                $('#alert').removeClass('success').addClass('error').html('Please select a future date.').fadeIn();
-                setTimeout(function(){ $('#alert').fadeOut() }, 4000);
-                 return false;
-             }
-
-
-
-                 // Check if the email address is valid
-            if (!isValidEmail(email)) {
-                $('#alert').removeClass('success').addClass('error').html('Please enter a valid email address.').fadeIn();
-                setTimeout(function(){ $('#alert').fadeOut() }, 4000);
-                return false;
-            }
-           
-
-            emailjs.init("Qur9J272yrutlMj77");
-
-            emailjs.send("service_zeodffs", "template_s6rjjmy", {
-                sendername: "Soundwave Symphony",
-                to: this.email.value,
-                subject: "New Booking Request",
-                message: "Thank you for booking a project with us. We appreciate your interest in working with us. \n\nYour details are as follows:\n\n" +
-                "Name: " + name + "\n" +
-                "Email: " + email + "\n" +
-                "Event Type: " + eventType + "\n" +
-                "Date: " + date + "\n" +
-                "Message: " + message + ".\n\n" +
-                "We will get back to you soon."
-   
-
-            }).then(function(response) {
-                console.log('SUCCESS!', response.status, response.text);
-                $('#alert').removeClass('error').addClass('success').html('Thank you! We will get back to you soon.').fadeIn();
-                $("#name").val('');
-                $("#email").val('');
-                $("#event-type").val('');
-                $("#date").val('');
-                $("#message").val('');
-                setTimeout(function(){ $('#alert').fadeOut() }, 4000);
-                    // Clear the form
- 
-            }, function(error) {
-                console.log('FAILED...', error);
-                $('#alert').removeClass('success').addClass('error').html('Error: Something went wrong. Please try again.').fadeIn();
-                setTimeout(function(){ $('#alert').fadeOut() }, 4000);
-            });
-            
-            return false; // Prevent the default form submission
-        });
-
-        // Email validation
-        function isValidEmail(email) {
-            var emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
-            return emailRegex.test(email);
+    /**
+     * Validates the Materialize select element.
+     */
+    function validateMaterializeSelect() {
+        let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+        let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+        if ($("select.validate").prop("required")) {
+            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
         }
-
-        $(".add-to-cart").click(function() {
-            let itemName = $(this).siblings("h3").text();
-            showCustomAlert(itemName + " has been added to your cart.");
-            // Here you can also add code to actually add the item to a shopping cart
-            // For example, updating a cart count, or sending an AJAX request to a server
-        });
-        
-
-        function showCustomAlert(message) {
-            $(".shopping-custom-alert").text(message).fadeIn();
-            setTimeout(function() {
-                $(".shopping-custom-alert").fadeOut();
-            }, 3000); // The alert will disappear after 3 seconds
-        }
-
-        $('.datepicker').datepicker();
-
-        document.addEventListener("DOMContentLoaded", function() {
-            $(".sidenav").sidenav({edge: "right"});
-            $(".collapsible").collapsible();
-            $(".tooltipped").tooltip();
-            $("select").formSelect();
-            $(".datepicker").datepicker({
-                format: "dd mmmm, yyyy",
-                yearRange: 3,
-                showClearBtn: true,
-                i18n: {
-                    done: "Select"
+        $(".select-wrapper input.select-dropdown").on("focusin", function () {
+            $(this).parent(".select-wrapper").on("change", function () {
+                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                    $(this).children("input").css(classValid);
                 }
             });
-        
-            validateMaterializeSelect();
-            function validateMaterializeSelect() {
-                let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
-                let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
-                if ($("select.validate").prop("required")) {
-                    $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
-                }
-                $(".select-wrapper input.select-dropdown").on("focusin", function () {
-                    $(this).parent(".select-wrapper").on("change", function () {
-                        if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
-                            $(this).children("input").css(classValid);
+        }).on("click", function () {
+            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+                $(this).parent(".select-wrapper").children("input").css(classValid);
+            } else {
+                $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
                         }
-                    });
-                }).on("click", function () {
-                    if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
-                        $(this).parent(".select-wrapper").children("input").css(classValid);
-                    } else {
-                        $(".select-wrapper input.select-dropdown").on("focusout", function () {
-                            if ($(this).parent(".select-wrapper").children("select").prop("required")) {
-                                if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
-                                    $(this).parent(".select-wrapper").children("input").css(classInvalid);
-                                }
-                            }
-                        });
                     }
                 });
             }
         });
-             
+    }
+});
+
+/**
+ * Smooth scrolling for links with hashes
+ * @param {Event} event - The click event
+ */
+$('a[href*="#"]').on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+    }
+});
