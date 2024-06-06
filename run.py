2de6d1eebb9fbf540294948bbb26b2047b229238
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2024-06-06 00:38:09 -0400

    create a reusable base html file and create route for each of my html files

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-from flask import Flask 
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    user_logged_in = True 
+    return render_template('index.html', is_index_page=user_logged_in)
+
+@app.route('/booking')
+def booking():
+    user_logged_in = False 
+    return render_template('form.html', is_index_page=user_logged_in)
+
+@app.route('/merchandise')
+def merchandise():
+    user_logged_in = False 
+    return render_template('merchandise.html', is_index_page=user_logged_in)
+
+
+if __name__ == "__main__":
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 5000)),
+            debug=True)         
+           # Run the app     
